6e91c0d74156ac66303613be82b84aa8ab56b500
Wilkes Joiner <wilkes@nasl_kbender.notifymd.local>
2009-02-10 15:52:44 -0600

    added readable output for stories

--- Main.hs
+++ Main.hs
@@ -32,7 +32,9 @@ token :: String -> String -> IO ()
 token username password = callRemote url opts (putStrLn . getToken . parseResponse)
     where url = "https://www.pivotaltracker.com/services/tokens/active"
           opts = [Curl.CurlUserPwd $ username ++ ":" ++ password]
-          getToken e = verbatim $ tag "token" /> tag "guid" /> txt $ e
+
+getToken :: Content -> String
+getToken e = verbatim $ tag "token" /> tag "guid" /> txt $ e
 
 projects :: String -> IO ()
 projects token = tokenCall url token (putRecord xml2Projects)
@@ -43,34 +45,51 @@ project token projectID = tokenCall url token (putRecord xml2Project)
     where url = projectURL ++ projectID
 
 stories :: String -> String -> IO ()
-stories token projectID = tokenCall url token (putRecord xml2Stories)
+stories token projectID = tokenCall url token (putStories . xml2Stories . parseResponse)
     where url = projectURL ++ projectID ++ "/stories"
 
 story :: String -> String -> String -> IO ()
-story token projectID storyID = tokenCall url token (putRecord xml2Story)
+story token projectID storyID = tokenCall url token (putStory . xml2Story . parseResponse)
     where url = projectURL ++ projectID ++ "/stories/" ++ storyID
 
 search :: String -> String -> String -> IO ()
-search token projectID filter = tokenCall url token (putRecord xml2Stories)
+search token projectID filter = 
+    tokenCall url token (putStories . xml2Stories . parseResponse)
     where url = projectURL ++ projectID ++ "/stories?filter=" ++ query
           query = escapeURIString isUnescapedInURI filter
 
 tokenCall :: String -> String -> (String -> IO ()) -> IO ()
 tokenCall url token callback = callRemote url opts callback
-    where opts = [Curl.CurlHttpHeaders ["X-TrackerToken: " ++ token, 
-                                        "Content-type: application/xml"]]
+    where opts = [Curl.CurlHttpHeaders ["X-TrackerToken: " ++ token, "Content-type: application/xml"]]
 
 callRemote :: String -> [Curl.CurlOption] -> (String -> IO ()) -> IO () 
 callRemote url opts callback = 
-    do response <- getResponse
-       case (Curl.respCurlCode response) of
-         Curl.CurlOK -> callback $ Curl.respBody response
-         _ -> fail $ Curl.respStatusLine response
-    where getResponse :: IO (Curl.CurlResponse_ [(String, String)] String)
-          getResponse = Curl.curlGetResponse_ url opts
+    do (code, res) <- Curl.curlGetString url opts
+       case code of
+         Curl.CurlOK -> callback res
+         _ -> fail $ show code
 
 putRecord :: (Show a) => (Content -> a) -> String -> IO ()
 putRecord transformer = putStrLn . show . transformer . parseResponse
+
+putStories :: [Story] -> IO ()
+putStories = mapM_ (\s -> putStrLn "" >> putStory s)
+
+putStory :: Story -> IO ()
+putStory story = 
+    do mapM_ display attrMap
+      where display (attr, label) = putStrLn $ label ++ ": " ++ (attr story)
+            attrMap = [(stName         ,"Name"),
+                       (stID           ,"ID"),
+                       (stType         ,"Type"),
+                       (stURL          ,"URL"),
+                       (stEstimate     ,"Estimate"),
+                       (stCurrentState ,"Status"),
+                       (stRequestedBy  ,"Requestor"),
+                       (stCreatedAt    ,"Created"),
+                       (stLabels       ,"Labels"),
+                       (stDescription  ,"Description")]
+
 
 parseResponse :: String -> Content
 parseResponse = content . (xmlParse "response")
